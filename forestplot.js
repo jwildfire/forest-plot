6301ee902c3059dc094ce3b5339551f1cedf6b4b
jwildfire <jwildfire@gmail.com>
2020-05-01 21:58:00 -0400

    added basic plots

--- forestplot.js
+++ forestplot.js
@@ -6,6 +6,42 @@ function forestplot(data, element, groups, pairs){
     chart.groups = groups;
     chart.pairs = pairs;
 
+    //data prep
+    chart.raw.forEach(function(d){
+        d.groups = groups.map(function(group){
+            return { 
+                key:group,
+                percent:d[group+"_percent"],
+                n: d[group + "_n"],
+                total: d[group+"_total"]
+            }
+        })
+        d.pairs = pairs.map(function (pair) { 
+            let pair_id = pair[0] + "_" + pair[1] 
+            return { 
+                key: pair_id, 
+                group1:pair[0],
+                group2:pair[1],
+                label:pair[0]+" vs. "+pair[1],
+                or: d[pair_id + "_or"],
+                p: d[pair_id+ "_pval"]
+            }
+        })
+    })
+
+    //define scales
+    let colorScale = d3.scale.ordinal()
+        .range(['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf'])
+        .domain(groups)
+
+    let all_percents = d3.merge(data.map(m => m.groups.map(n => n.percent)))
+    let percent_extent = d3.extent(all_percents)
+    let groupScale = d3.scale.linear().range([10, 110]).domain(percent_extent)
+
+    let all_ors = d3.merge(data.map(m => m.pairs.map(n => n.or)))
+    let or_extent = d3.extent(all_ors)
+    let orScale = d3.scale.linear().range([10, 110]).domain(or_extent)
+
     console.log(pairs)
     //header   
     
@@ -15,40 +51,156 @@ function forestplot(data, element, groups, pairs){
         chart.head1=chart.head.append("tr")
         chart.head1.append("td")
         chart.head1.append("td")
-        chart.head1.append("td").text("Groups").attr("colspan",groups.length)
-        chart.head1.append("td").text("Comparisons").attr("colspan", pairs.length)
+        chart.head1.append("td").text("Groups").attr("colspan",groups.length+1)
+        chart.head1.append("td").text("Comparisons").attr("colspan", pairs.length+1)
 
         chart.head2 = chart.head.append("tr")
-        chart.head2.append("th").text("SOC")
+        chart.head2.append("th").text("System Organ Class")
         chart.head2.append("th").text("Preferred Term")
         chart.head2.selectAll("th.group").data(groups).enter().append("th").text(d=>d)
+        var groupAxis = d3.svg.axis().scale(groupScale).ticks(6).orient("top");
+        chart.head2.append("th")
+            .style('padding-right', 10)
+            .style('padding-left', 10)
+            .append('svg')
+            .attr('height', 20)
+            .attr('width', 120)
+            .append('svg:g')
+            .attr('class', 'axis percent')
+            .attr("transform", "translate(0,20)")
+            .call(groupAxis)
+            
+
         chart.head2.selectAll("th.pairs").data(pairs).enter().append("th").text(d => d[0]+" vs."+d[1])
+        var orAxis = d3.svg.axis().scale(orScale).ticks(6).orient("top");
+        chart.head2.append("th")
+            .style('padding-right', 10)
+            .style('padding-left', 10)
+            .append('svg')
+            .attr('height', '20')
+            .attr('width', 100)
+            .append('svg:g')
+            .attr('class', 'axis percent')
+            .attr("transform", "translate(0,20)")
+            .call(orAxis);
 
         chart.body = chart.table.append("tbody")
         chart.rows = chart.body.selectAll("tr").data(data).enter().append("tr")
-        chart.rows.append("td").attr("class","soc").text(d=>d.soc) 
-        chart.rows.append("td").attr("class", "term").text(d => d.term)
+        chart.rows.append("td").attr("class","soc")
+        .text(d => d.soc.length > 25 ? d.soc.substring(0,25)+"...": d.soc) 
+        .attr("title",d=>d.soc)
+        chart.rows.append("td").attr("class", "term")
+        .text(d => d.term.length > 25 ? d.term.substring(0,25) + "..." : d.term)
+        .attr("title", d => d.term)
+
+
         //Group Counts
-        groups.forEach(function(group){
-            chart.rows.append("td").attr("class", "group-count")
-            .style("text-align", "center")
-            .text(d=>d[group+"_percent"]+"%")
-            .attr("title", d => d[group + "_n"] + "/" + d[group + "_total"])
-            .style("cursor","pointer")
-        })
+        chart.rows.selectAll("td.group-count")
+        .data(d=>d.groups)
+        .enter()
+        .append("td")
+        .attr("class", "group-count")
+        .style("text-align", "center")
+        .text(d=>d.percent+"%")
+        .attr("title", d => d.n + "/" + d.total)
+        .style("cursor","help")
+        .style("color",d=>colorScale(d.key))
+        
+
+        //group plot
+        chart.groupPlot = chart.rows.append("td","group-plot").append("svg").attr("height",20).attr("width",120)
+        chart.groupPlot.selectAll("circle")
+        .data(d=>d.groups)
+        .enter()
+        .append("circle")
+        .attr("cx",d=>groupScale(d.percent))
+        .attr("cy",10)
+        .attr("r",5)
+        .attr("stroke",d=>colorScale(d.key))
+        .attr("fill", d=>colorScale(d.key))
+        .style("cursor","help")
+        .append("title").text(d=>d.key+": "+d.percent+"% ("+d.n+"/"+d.total+")")
+
 
         //Group Comparisons
-        pairs.forEach(function(pair){
-            let pair_or = pair[0] + "_" + pair[1] + "_or"
-            let pair_p = pair[0] + "_" + pair[1] + "_pval"
-            console.log(pair_or)
-            chart.rows.append("td").attr("class","compare")
-            .style("text-align", "center")
-            .text(d=>d[pair_or] ? d[pair_or] : "-")
-            .attr("title",d=>"p="+d[pair_p])
-            .style("font-weight",d=>d[pair_p]<0.05 ? "bold" : null) 
-            .style("color", d => d[pair_p] < 0.05 ? "black" : "#ccc") 
-        })
+        chart.rows
+        .selectAll("td.compare")
+        .data(d=>d.pairs)
+        .enter()
+        .append("td")
+        .attr("class","compare")
+        .style("text-align", "center")
+        .text(d=>d.or ?d.or : "-")
+        .attr("title",d=>"p="+d.p)
+        .style("font-weight",d=>d.p<0.05 ? "bold" : null) 
+        .style("color", d => d.p < 0.05 ? "black" : "#ccc") 
+        
+       
+        var diffPlots = chart.rows.append("td")
+            .attr('class','diffplot')
+            .append('svg')
+            .attr('height', 20)
+            .attr('width', 120)
+            .append('g')
+
+        
+
+        var diffPoints = diffPlots.selectAll('g').data(d=>d.pairs.filter(f=>f.or)).enter().append('g');
+        diffPoints.append('title').text(d=>d.label+": "+d.or+" (p="+d.p+")");
+        //Append graphical rate differences.
+        var triangle = d3.svg
+            .line()
+            .x(function (d) {
+                return d.x;
+            })
+            .y(function (d) {
+                return d.y;
+            })
+            .interpolate('linear-closed');
+
+        diffPoints
+            .append('svg:path')
+            .attr('d', function (d) {
+                var h =20,
+                    r = 5;
+
+                var leftpoints = [
+                    { x: orScale(d.or), y: h / 2 + r }, //bottom
+                    { x: orScale(d.or) - r, y: h / 2 }, //middle-left
+                    { x: orScale(d.or), y: h / 2 - r } //top
+                ];
+                return triangle(leftpoints);
+            })
+            .attr('class', 'diamond')
+            .attr('fill-opacity', function (d) {
+                return d.p <0.05 ? 1 : 0.1;
+            })
+            .attr('fill', d=>colorScale(d.group1))
+            .attr('stroke', d => colorScale(d.group1))
+            .attr('stroke-opacity', 0.3);
+
+        diffPoints
+            .append('svg:path')
+            .attr('d', function (d) {
+                var h = 20;
+                    r = 5;
+
+                var rightpoints = [
+                    { x: orScale(d.or), y: h / 2 + r }, //bottom
+                    { x: orScale(d.or) + r, y: h / 2 }, //middle-right
+                    { x: orScale(d.or), y: h / 2 - r } //top
+                ];
+                return triangle(rightpoints);
+            })
+            .attr('class', 'diamond')
+            .attr('fill-opacity', function (d) {
+                return d.p < 0.05 ? 1 : 0.1;
+            })
+            .attr('fill', d => colorScale(d.group2))
+            .attr('stroke', d => colorScale(d.group2))
+            .attr('stroke-opacity', 0.3);
+
+
 
         $('#container table').DataTable({ "paging": false, "order": [[2, "desc"]]});
     }
