c403d7f4b02a0fa390a5bc46d4249140f56320ec
jwildfire <jwildfire@gmail.com>
2020-10-22 12:14:47 -0400

    add filter for incidence and comparisons. #2 #5

--- forestplot.js
+++ forestplot.js
@@ -1,6 +1,8 @@
 function forestplot(data, element, groups, pairs){
     let chart = {}
     chart.wrap = d3.select(element)
+    chart.config= {};
+    chart.controls = d3.select(element).append("div").attr("class","controls")
     chart.table = chart.wrap.append("table")
     chart.raw = data;
     chart.groups = groups;
@@ -51,7 +53,7 @@ function forestplot(data, element, groups, pairs){
         chart.head1=chart.head.append("tr")
         chart.head1.append("th")
         chart.head1.append("th")
-        chart.head1.append("th").text("Groups").attr("colspan",groups.length+1)
+        chart.head1.append("th").text("Incidence").attr("colspan",groups.length+1)
         chart.head1.append("th").text("Comparisons").attr("colspan", pairs.length+1)
 
         chart.head2 = chart.head.append("tr")
@@ -105,7 +107,7 @@ function forestplot(data, element, groups, pairs){
         .append("td")
         .attr("class", "group-count")
         .style("text-align", "center")
-        .text(d=>d.percent+"%")
+        .text(d=>d.percent)
         .attr("title", d => d.n + "/" + d.total)
         .style("cursor","help")
         .style("color",d=>colorScale(d.key))
@@ -206,9 +208,77 @@ function forestplot(data, element, groups, pairs){
 
 
 
-        $('#container table').DataTable({ "paging": false, "order": [[2, "desc"]]});
+        let table = $('#container table').DataTable({ 
+            "dom": '<"top"if>rt<"clear">',
+            "paging": false, 
+            "order": [[2, "desc"]]
+        });
+
+        // make controls
+        let indidenceControl = chart.controls.append("div").attr("class","slider-wrap")
+        indidenceControl.append("label").attr("id","incidence-label").text("Incidence: ")
+        indidenceControl.append("span").attr("id","incidence-vals").attr("class","label").text("0 - "+Math.ceil(percent_extent[1]))
+        indidenceControl.append("div").attr("id","incidence-slider")
+        chart.config.incidenceFilter=[0,Math.ceil(percent_extent[1])]
+        $("#incidence-slider").slider({
+            range: true,
+            min: 0,
+            max: Math.ceil(percent_extent[1]),
+            values: [0,Math.ceil(percent_extent[1])],
+            slide: function(event, ui) {
+                d3.select("#incidence-vals").text(ui.values[0] + " - " + ui.values[1]);
+                chart.config.incidenceFilter=ui.values
+                table.draw();
+            }
+        });
+
+        let compControl = chart.controls.append("div").attr("class","slider-wrap")
+        compControl.append("label").attr("id","comp-label").text("Comparisons: ")
+        compControl.append("span").attr("id","comp-vals").attr("class","label").text("0 - "+Math.ceil(or_extent[1]))
+        compControl.append("div").attr("id","comp-slider")
+        chart.config.compFilter=[0,Math.ceil(or_extent[1])]
+        $("#comp-slider").slider({
+            range: true,
+            min: 0,
+            max: Math.ceil(or_extent[1]),
+            values: [0,Math.ceil(or_extent[1])],
+            slide: function(event, ui) {
+                d3.select("#comp-vals").text(ui.values[0] + " - " + ui.values[1]);
+                chart.config.compFilter=ui.values
+                table.draw();
+            }
+        });
+
+        // Search on rates
+        $.fn.dataTable.ext.search.push(
+            function( settings, data, dataIndex ) {
+                let incidence_vals = data.filter(function(d,i){
+                    let first_col = 2
+                    let last_col = first_col + groups.length
+                    return i>=first_col & i <last_col
+                })
+                let incidence_max = d3.max(incidence_vals, d=>+d);
+                let incidence_flag = incidence_max >= chart.config.incidenceFilter[0] & incidence_max<=chart.config.incidenceFilter[1]
+
+                let comp_vals = data.filter(function(d,i){
+                    let first_comp = 2 + groups.length + 1
+                    let last_comp = first_comp + pairs.length
+                    return i>=first_comp & i < last_comp
+                })
+                let comp_max = d3.max(comp_vals, d=>d=="-"?0:+d);
+                let comp_flag = comp_max >= chart.config.compFilter[0] & comp_max<=chart.config.compFilter[1]
+
+            return comp_flag & incidence_flag
+            }
+        );
+
+
+
     }
 
     chart.draw(chart.raw,groups, pairs)
-    d3.select(table).classed("data-table")
+
+
+
+
 }
